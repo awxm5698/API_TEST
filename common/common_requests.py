b4bf08b2688375b1d3f2623259e3d3cbea188604
awxm5698 <45061306+awxm5698@users.noreply.github.com>
2018-12-26 13:34:24 +0800

    Update common_requests.py

--- common/common_requests.py
+++ common/common_requests.py
@@ -1,57 +1,67 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 # -*- coding:utf-8 -*-
 # os.path.split(os.path.realpath(__file__))[0]  # 打印当前文件的绝对路径,./a/a.py调用./b/b.py,打印./b
 # os.path.split(os.path.abspath(sys.argv[0]))[0] # 打印执行文件的绝对路径，./a/a.py调用./b/b.py,打印./a
-			
-import requests,sys
+import sys,os
+import requests
+sys.path.append(os.path.dirname(os.getcwd()))
 from requests import exceptions
-from requests_toolbelt import SSLAdapter
-import sys,os,re
-#path = os.path.split(os.path.abspath(sys.argv[0]))[0]
+from common.common_log import Common_Log
 
-sys.dont_write_bytecode = True
+class Common_Requests:
+	def __init__(self, _url, _port=None):
 
-
-class testCommon():
-	def __init__(self,url,port=None):
-		if port == "" or port == None:
-			self.url = url
+		if _port == None:
+			self.url = _url
 		else:
-			self.url = url + ":"+port
-		
-	def get(self,path,wordbook):
-		try:
-			headers={'Connection':'close'}
-			requests.packages.urllib3.disable_warnings()
-			response = requests.get(self.url + path, params = wordbook,headers = headers, verify=False)
-			#response.encoding = 'utf-8'
-			#print response.url
-			#print response.cookies
-			#print response.json()
-			return response #获取服务器返回的页面信息
-		except exceptions.HTTPError as e:
-			return 'Error: ',e
-			
-	def post(self,path,wordbook):
-		try:
-			headers = {'Content-Type':'application/json;charset=utf-8'}
-			response = requests.post(self.url + path, data = wordbook,verify=False)
-			#print response.status_code
-			#print response.headers
-			#print response.cookies
-			return response
-		except exceptions.HTTPError as e:
-			return 'Error: ',e
-if __name__ == "__main__":
-	url = "http://168.61.148.253"
-	port = "8080"
-	year = '2017'
-	path = '/item/vehicle/makes/'+year
-	wordbook = ''
-	t = testCommon(url,port)
-	response = t.get(path,wordbook)
-	print response.json()
-	
+			self.url = _url + ":" + _port
+		self._info = Common_Log()
 
-	
-	
+	def get(self, _path='/', _from_data=None):
+		try:
+			item = "from_data=" + str(_from_data)
+			self._info.log(item)
+			headers = {'Connection': 'close'}
+			# requests.packages.urllib3.disable_warnings()
+			_response = requests.get(self.url + _path, params=_from_data, headers=headers, verify=False)
+			try:
+				self._info.log("get_url="+_response.url)
+				self._info.log("response_code= " + str(_response.status_code))
+				# self._info.log("response=" + str(_response.json()))
+			except:
+				self._info.log("response is not json or has a unknown error")
+			_response.close()
+			return _response  # 获取服务器返回的页面信息
+		except exceptions.HTTPError as e:
+			_response.close()
+			self._info.e_log(e)
+			return 'Error: ', e
+
+	def post(self, _path='/', _from_data=None):
+		try:
+			item = "from_data=" + str(_from_data)
+			self._info.log(item)
+			headers = {'Content-Type': 'application/json;charset=utf-8'}
+			_response = requests.post(self.url + _path, json=_from_data, headers=headers, verify=False)
+			try:
+				self._info.log("post_url="+_response.url)
+				self._info.log("response_code= " + str(_response.status_code))
+				# self._info.log("response=" + str(_response.json()))
+			except:
+				self._info.log("response is not json or has a unknown error")
+			_response.close()
+			return _response
+		except exceptions.HTTPError as e:
+			_response.close()
+			self._info.e_log(e)
+			return 'Error: ', e
+
+
+if __name__ == "__main__":
+	url = "http://www.baidu.com"  # type: str
+	t = Common_Requests(url)
+	t.post()
+	l = Common_Log()
+	l.log("admin")
+
+	sys.exit()
