2b13c0692e91b11ea08b5d484f6e3b0f02168270
awxm5698 <45061306+awxm5698@users.noreply.github.com>
2018-12-26 13:40:38 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 #coding=utf-8
 
-import  unittest
-import HTMLTestRunner
+import unittest
+from mylib import HTMLTestRunner3
 import time
 import os
 import smtplib
@@ -34,33 +34,34 @@ def send_mail(file_new):
     smtp.login(emailFrom,emailFromPassWord)
     smtp.sendmail(emailFrom,emailTo,msg.as_string())
     smtp.quit()
-    print"send success!"
+    print("send success!")
 
 # 查找测试目录，找到最新生成的测试报告
 def new_report(test_report):
     lists = os.listdir(test_report)
-    lists.sort(key=lambda fn:os.path.getatime(test_report+'\\'+fn))
+    lists.sort(key=lambda fn:os.path.getatime(test_report+'/'+fn))
     file_new = os.path.join(test_report,lists[-1])
     return file_new
 #################################################################
 # ---------------------------执行用例-------------------------- #
 #################################################################
 if __name__=="__main__": # 请点击左侧箭头执行脚本
-    test_dir = os.path.split(os.path.realpath(__file__))[0]+"\\test_case"
-    test_report = os.path.split(os.path.realpath(__file__))[0]+"\\test_report\\"
+    test_dir = os.path.split(os.path.realpath(__file__))[0]+"/test_case"
+    test_report = os.path.split(os.path.realpath(__file__))[0]+"/test_report/"
 
     #################################################################
     # --------------根据用例名称匹配需要执行的用例----------------- #
     #################################################################
-    discover = unittest.defaultTestLoader.discover(test_dir, pattern='test_API_GET.py')
+    discover = unittest.defaultTestLoader.discover(test_dir, pattern='test_a_demo.py')
+    # print(discover)
 
     # -----*------*-----*-----*------执行测试用例并写报告-----*------*-----*-----*------
     filename = test_report + timestr + "_testResult.html"
     fp = open(filename, "wb")
-    runner = HTMLTestRunner.HTMLTestRunner(stream=fp,
-                                           title="API TEST REPORT",
-                                           description="TESTCASE RUNNING REPORT:")
+    runner = HTMLTestRunner3.HTMLTestRunner(stream=fp,
+                                            title="API TEST REPORT",
+                                            description="TESTCASE RUNNING REPORT:")
     runner.run(discover)
     fp.close()
     new_report = new_report(test_report)
-    send_mail(new_report) # 发送邮件
+    # send_mail(new_report)
